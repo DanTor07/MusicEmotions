2ac0e879fea743c175251db225183dbe87e77833
DanTor07 <125132273+DanTor07@users.noreply.github.com>
2023-11-06 17:17:53 -0500

    Proyecto Final: Técnicas de Programación II Music Emotion's

---
+++ Emotion.cs
@@ -0,0 +1,8 @@
+﻿namespace Borrador3Proyecto
+{
+    class Emotion
+    {
+        public string Emoción { get; set; }
+        public List<MusicInfo> MusicList { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Borrador3Proyecto
+{
+    class Program
+    {
+        static void Main()
+        {
+            EmotionApp.Run();
+        }
+    }
+}
